1a5440f59b2039eac21f6fef8bb0bd01fc1012e1
Sergey Abu <iteelion.dev@gmail.com>
2024-05-10 20:57:00 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui /opt/sd
+
+chown -R sd:sd /opt/sd && echo 'venv_dir=./venv' > webui-user.sh
+
+install_dir=./
+
+./webui.sh -f can_run_as_root --exit --skip-torch-cuda-test
+
+VIRTUAL_ENV=/venv
+
+PATH="$VIRTUAL_ENV/bin:$PATH"
