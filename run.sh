579c0e7128c14f5df5c7f94533bde4d8c1c39ec6
Sergey Abu <iteelion.dev@gmail.com>
2024-05-11 14:58:44 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,1 @@
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
-
-chown -R sd:sd /opt/sd/stable-diffusion-webui
-
-cd ./stable-diffusion-webui
-
-echo 'venv_dir=./venv' > ./webui-user.sh
-
-install_dir=./
-
-./webui.sh -f can_run_as_root --exit --skip-torch-cuda-test
-
-VIRTUAL_ENV=/venv
-
-PATH="$VIRTUAL_ENV/bin:$PATH"
+echo "test"
