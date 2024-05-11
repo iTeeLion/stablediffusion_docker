78a9e4edeb557c5d37c02c9cfa7ff6933b7e3544
Sergey Abu <iteelion.dev@gmail.com>
2024-05-11 20:53:24 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,6 +6,8 @@ if [ ! -d /opt/sd/webui/venv ]; then
     git init
     git pull https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
     ./webui.sh --skip-torch-cuda-test --exit
+    pip3 install --upgrade pip && pip install packaging numexpr
 fi
 
 python3 launch.py --listen $1
+
