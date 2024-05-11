2982fc3af1a256a9370bbbfcde654ee63e9bdcf6
Sergey Abu <iteelion.dev@gmail.com>
2024-05-11 15:46:32 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
-while true
-do
-  echo "test"
-  sleep 30
-done
+#!/bin/bash
+
+if [ ! -d /opt/sd/webui ]; then
+  git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git ./webui
+  cd ./webui
+  ./webui.sh --skip-torch-cuda-test --exit
+fi
+
+cd /opt/sd/webui
+
+python3 launch.py --listen $1
