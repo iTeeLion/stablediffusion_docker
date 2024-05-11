3010a881a926e1cef4edfde7391ae53c2fe290f3
Sergey Abu <iteelion.dev@gmail.com>
2024-05-11 18:15:01 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 
 if [ ! -d /opt/sd/webui/venv ]; then
   git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
-  sudo -u sd cp -r ./stable-diffusion-webui/* ./webui
+  mv -r ./stable-diffusion-webui/* ./webui
+  rm -rf ./stable-diffusion-webui/
   cd ./webui
-  sudo -u sd ./webui.sh --skip-torch-cuda-test --exit
+  ./webui.sh --skip-torch-cuda-test --exit
 fi
 
 cd /opt/sd/webui
 
-sudo -u sd python3 launch.py --listen $1
+python3 launch.py --listen $1
