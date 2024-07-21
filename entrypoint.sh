bdcd8a360c0d8098bfebc309cf513a5ffc8df6f4
Sergey Abu <iteelion.dev@gmail.com>
2024-07-22 02:42:04 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd /opt/sd
-
 if [ ! -d /opt/sd/webui/venv ]; then
-    git config --global --add safe.directory /opt/sd
-    git init
-    git pull https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+    mkdir -p /opt/sd/_dist && cd /opt/sd/_dist
+    git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+    cp -r /opt/sd/_dist/stable-diffusion-webui/* /opt/sd
+    cd /opt/sd
+    rm -rf /opt/sd/_dist
     /opt/sd/webui.sh --skip-torch-cuda-test --exit
     pip3 install --upgrade pip && pip3 install packaging numexpr
 fi
 
+cd /opt/sd
 python3 launch.py --listen $1
-
